3aa12a8bcec87b19ae881ff1390eb04ca9e5b855
ipon <iponsarif@gmail.com>
2019-02-28 13:46:09 +0700

    preparing for heroku deploy

--- app.py
+++ app.py
@@ -1,25 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import jwt
 from src.routes import router
 
 app = Flask(__name__)
 app.register_blueprint(router)
 
-@app.route('/encode')
-def jwtEncode():
-    encoded = jwt.encode({"data": "makers"}, "kucing-merah", algorithm="HS256")
-    return encoded
-
-a = "bisa ga ini coy di debug"
-print(a)
-@app.route('/decode', methods=["POST"])
-def jwtDecode():
-    decoded = jwt.decode(request.json["token"], "kucing-merah", algorithms=["HS256"])
-    return str(decoded)
-
-# @app.errorhandler(403)
-# def errorhandler403(e):
-#     message = {
-#         "status-code"
-#     }
-#     return "error 403"
+@app.route('/penjumlahan/<int:firstNumber>/<int:secondNumber>')
+def penjumlahan(firstNumber, secondNumber):
+    return jsonify({
+        "hasil_jumlah": firstNumber+secondNumber,
+        "tv_one": "memang beda"
+    })
